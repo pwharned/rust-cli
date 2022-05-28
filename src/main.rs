deb7fa5fe112ba544c73491c90473e37ac9b3b19
pwharned <pwharned@gmail.com>
2022-05-28 14:20:02 -0700

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+
 fn main() {
+    let pattern = std::env::args().nth(1).expect("no pattern given");
     println!("Hello, world!");
+    println!("{}",pattern);
+
 }
